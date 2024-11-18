ea674ea978fa82b299daa248529674759e3b817c
Baatarvant <baatarvant@gmail.com>
2024-11-18 11:55:04 +0800

    removed auth

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,5 +1,4 @@
 import { UserModel } from "@/lib/models";
-import bcrypt from "bcrypt";
 
 export const GET = async () => {
   try {
@@ -8,30 +7,5 @@ export const GET = async () => {
     return Response.json({ users });
   } catch (error) {
     return Response.json({ message: error });
-  }
-};
-
-export const POST = async (request: Request) => {
-  const { email, password, firstName, lastName } = await request.json();
-  try {
-    const userFound = await UserModel.findOne({ email });
-    if (userFound) {
-      return {
-        error: "Email already exists!",
-      };
-    }
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new UserModel({
-      firstName,
-      lastName,
-      email,
-      password: hashedPassword,
-    });
-    const savedUser = await user.save();
-
-    return Response.json({ user: savedUser });
-  } catch (e) {
-    console.log(e);
-    return Response.json({});
   }
 };
